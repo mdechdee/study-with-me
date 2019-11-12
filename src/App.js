22c034d5bb73ccbdb8e9dba2dd31193a51e12a04
mdechdee <dechdee.m@gmail.com>
2019-11-12 20:17:00 +0900

    Create page wrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import 'react-bootstrap/dist/react-bootstrap.min.js';
 import logo from './logo.svg';
 import './App.css';
+import './scss/_base.scss';
 
 class App extends React.Component{
   render() { 
     return(
-      <Container fluid>
+      <Container fluid className>
         <Row className='justify-content-sm-center outer-wrap'>
-          <div className="App">
-            <h1> "HI" </h1>
-          </div>
+          <Col xs={12} sm={8} md={6} lg={4} className='page-wrap'>
+            <div className='horizontal-div'>
+              <Row className='justify-content-center'>
+                <Col><h1>"HI"</h1></Col>
+                <Col><h1>"HI"</h1></Col>
+                <Col><h1>"HI"</h1></Col>
+                <Col><h1>"HI"</h1></Col>
+              </Row>
+            </div>
+          </Col>
         </Row>
       </Container>
     );
