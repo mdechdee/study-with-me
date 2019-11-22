714b5fd3aa5a37ad43f096add642e6099b572537
Wirittipol Supdateyarnnakorn <kws.stupid@gmail.com>
2019-11-22 17:40:11 +0900

    Add create group

---
+++ src/CreateGroup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+class CreateGroup extends React.Component {
+
+	render(){
+		return(
+
+			<div>
+				<div>
+				Groups in SWM
+				</div>
+
+        <div>
+
+
+
+				</div>
+
+			</div>
+		);
+	}
+}
+
+export default CreateGroup;
--- src/FindGroups.js
+++ src/FindGroups.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 class FindGroups extends React.Component {
+
 	render(){
 		return(
 
-			<div> b </div>
+			<div>
+				<div>
+				Groups in SWM
+				</div>
+				<div>
+					<Row>
+						<Col>
+						NAME
+						</Col>
+						<Col>
+						START AT
+						</Col>
+						<Col>
+						TIME
+						</Col>
+						<Col>
+						MEMBER
+						</Col>
+					</Row>
+					<Row>
+					</Row>
+
+
+					<div>
+						<Row>
+							<Col>
+								<Link to='/CreateGroup'>
+									<Button>Create a group</Button>
+								</Link>
+							</Col>
+						</Row>
+					</div>
+				</div>
+
+
+			</div>
 		);
 	}
 }
