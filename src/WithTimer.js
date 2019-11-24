e5696591ecacac48c66c43e49ccc3cbbe90ebc22
mdechdee <dechdee.m@gmail.com>
2019-11-24 19:26:42 +0900

    Update WithTimer.js

--- src/WithTimer.js
+++ src/WithTimer.js
@@ -97,12 +97,9 @@ const withTimer = (Component) =>
 					//<div> {new Date(this.state.stopTime).getMilliseconds()} </div>
 		render(){
 			return(
-				<AuthContext.Consumer>{ auth => { return(
-					<TimerContext.Provider auth={auth} value={this.state}>
+					<TimerContext.Provider value={this.state}>
 						<Component {...this.props} />
 					</TimerContext.Provider>
-				)}}
-				</AuthContext.Consumer>
 			);
 		}
 	}
