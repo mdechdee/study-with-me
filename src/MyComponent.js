12561f44ee3e71513e57d260d69b65e4038bccaf
Wirittipol Supdateyarnnakorn <kws.stupid@gmail.com>
2019-11-24 21:34:29 +0900

    Add handle click when click a group

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,19 +2,25 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap';
 // import Moment from 'moment';
 import './scss/FindGroups.scss';
+import {db} from './firebase/firebase.js'
 
 class MyComponent extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
+	handleClick() {
+		var userRef = db.ref(`users/${this.props.uid}`);
+		
+	}
+
 	render() {
 		return(
 			<div>
 				<Row className='group-row'>
-				
+
 					<Col sm={3}>
-						<Row><div className='group-name'> {this.props.one.name} </div></Row>
+						<Row><div className='group-name' onClick={this.handleClick}>{this.props.one.name} </div></Row>
 					</Col>
 
 					<Col sm={3}>
