f9731e4b3d85b178312f6aef16c6f6c73f6fb4e9
bosskab123 <phatarapran7524@gmail.com>
2019-11-26 07:52:37 +0900

    Update Carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,14 +7,12 @@ class Carousel extends React.Component{
     super(props);
     this.state ={
       url : "",
-      progress : "",
-      goal:"",
-      info: null,
-      name: "",
-      numberLargeSmile:0,
-      numberSmile:0,
-      numberLike:0,
-      numberLove:0
+      progress : [],
+      goal:[],
+      peopleName:[],
+      Name:[],
+      people: [],
+      isReady: false
     }
   }
 
@@ -23,43 +21,63 @@ class Carousel extends React.Component{
       .then(url => {
         this.setState({url: url});
       }).catch(function(error) {
-      console.log(error);
+      console.log("Not found");
     });;
   }
+  fetchPeople(){
+  	db.ref(`groups/${this.props.groupName}/people`).once('value',(snapshot) => {
+  			let val = snapshot.val()
+  			console.log("totot")
+			console.log(val)
+			Object.keys(val).forEach((item) => {
+				this.setState({people: [...this.state.people,[item,snapshot.val()[item]]]});
+			})
+			this.setState({isReady:true})
+		})
+  }
+  fetchName(){
+  	db.ref(`users`).once('value',(snapshot) => {
+			let val = snapshot.val();
+			console.log(val)
+			Object.keys(val).forEach((item) => {
+				this.setState({peopleName: [...this.state.peopleName, [item,val[item].name]]});
+			})
+
+		})
+  }
+  preparePeopleInfo(){
+  	for(let i=0; i<this.state.people.length; i+=1){
+  		let person = this.state.people[i]
+  		console.log(i)
+  		this.setState({goal: [...this.state.goal, person[1].goal]})
+  		for(let j=0; j<this.state.peopleName.length; j+=1){
+  			let p = this.state.peopleName[j]
+  			if(person[0]==p[0]){
+  				this.setState({Name:[...this.state.Name,p[1]]})
+  				break;
+  			}
+  		}
+ 	}
+  }
   componentDidMount(){
-    console.log("Carousel/componentDidMount")
-    var self=this;
-    db.ref(`groups/${this.props.groupName}/people`).child(`/${this.props.mapPeopleWithNumber[this.props.rank]}`).once('value',
-      function(snapshot){
-        self.setState({
-          goal:snapshot.val().goal,
-          progress:snapshot.val().progress,
-          numberLargeSmile:snapshot.val().numberLargeSmile,
-          numberSmile:snapshot.val().numberSmile,
-          numberLike:snapshot.val().numberLike,
-          numberLove:snapshot.val().numberLove,
-        });
-    })
-    db.ref("users").child(`/${this.props.mapPeopleWithNumber[this.props.rank]}`).on('value',function(snapshot){
-        var value = snapshot.val();
-        self.setState({name:value.name});
-      })
-    }
+  	this.fetchName()
+  	setTimeout(this.fetchPeople(), 1000)
+  }
   render(){
     this.getUrl()
     console.log("Carousel/render")
-    console.log(this.props.mapPeopleWithNumber[this.props.rank])
-    console.log(this.props.intervalNum)
-    return (
+    console.log(this.state)
+        return (
       <MDBCol>
         <MDBCard style={{ width: "15rem"  }}>
           <MDBCardImage className="img-fluid" src={ this.state.url || "https://via.placeholder.com/200x300"} waves />
           <MDBCardBody>
-            <MDBCardTitle>{this.state.name}</MDBCardTitle>
+            <MDBCardTitle>{this.state.Name[this.props.rank]}</MDBCardTitle>
             <MDBCardText>
-              {this.state.progress||"No info"}
+              progress
               <br/>
-              {this.state.goal||""}
+              {this.state.goal[this.props.rank]}
+              rank: {this.props.rank}
             </MDBCardText>
             <div>
               <Cheer uid={this.props.mapPeopleWithNumber[this.props.rank]} intervalNum = {this.props.intervalNum} groupName = {this.props.groupName}/>
