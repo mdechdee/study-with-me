558007fc9d7e622f49eae104e6fef363f7099ee1
mdechdee <dechdee.m@gmail.com>
2019-11-26 01:17:09 +0900

    Embed timer to MyGroup

--- src/MyGroup.js
+++ src/MyGroup.js
@@ -12,7 +12,7 @@ import { MDBContainer, MDBCarousel ,MDBBtn, MDBCard, MDBCardBody, MDBCardImage, 
 import './scss/MyGroup.scss';
 
 class MyGroup extends React.Component {
-
+	static contextType = TimerContext
 	constructor(props){
 		super(props);
 		this.state = {
@@ -34,97 +34,39 @@ class MyGroup extends React.Component {
 		this.handleRight = this.handleRight.bind(this);
 	}
 	// bring data from database
-	fetchGroupName(){
-		db.ref(`users/${this.props.uid}`).once('value',(snapshot) =>{
-			console.log("mygroup/fetchGroupName : users/uid");
-			console.log(snapshot.val());
-			this.setState({
-				groupName: snapshot.val().group
-			})
-		})
-	}
+	
 	collectPeople(){
-		db.ref(`groups/${this.state.groupName}/people`).once('value',(snapshot) =>{
+		db.ref(`groups/${this.props.timer.group}/people`).once('value',(snapshot) =>{
 			this.setState({
 				people: snapshot.val()
 			})
+			let num = 0;
+			let temp = {};
+			console.log(this.state.people)
+			Object.keys(this.state.people).forEach(function (person){
+				temp[num]=person;
+			    num=num+1;
+			});
+			this.setState({
+				mapPeopleWithNumber:temp,
+				totalPeople:num,
+				isLoaded:true
+			});
+			console.log("mygroup/countpeople : state");
+			console.log(this.state);
 		})
 	}
 	// map uid to number and count total number of people
-	countPeople(){
-		let num = 0;
-		let temp = {};
-		Object.keys(this.state.people).forEach(function (person){
-			temp[num]=person;
-		    num=num+1;
-		});
-		this.setState({mapPeopleWithNumber:temp});
-		this.setState({totalPeople:num});
-		this.setState({isLoaded:true});
-		console.log("mygroup/countpeople : state");
-		console.log(this.state);
-	}
+
 	handleLeft(){
 		if (this.state.rank>0) this.setState({rank:this.state.rank-1})
 	}
 	handleRight(){
 		if (this.state.rank<this.state.totalPeople-1) this.setState({rank:this.state.rank+1})
 	}
-	//Fetch group start/stop/interval time (Now start with current time)
-	fetchGroupData(){
-		db.ref(`groups/${this.state.groupName}`).once('value', (snapshot) => {
-			let val = snapshot.val();
-			this.setState({
-	        	intervalTime: val.interval,
-	        	startTime: this.state.currentTime,
-        		stopTime: this.state.currentTime + val.interval
-	    	});
-        })
 
-	}
-	// Fetch current server's time
-	fetchCurrentTime(){
-        db.ref('/.info/serverTimeOffset').on('value', (data) => {
-	    	this.setState({
-				offset : data.val(),
-				currentTime: this.state.offset + Date.now()
-			})
-	  	});
-	}
-
-
-	pushNewStartTime(){
-		this.setState({
-	        startTime: this.state.startTime + this.state.intervalTime,
-        	stopTime: this.state.stopTime + this.state.intervalTime
-        })
-		db.ref(`groups/${this.state.groupName}/startTime`).set(this.state.startTime)
-		db.ref(`groups/${this.state.groupName}/stopTime`).set(this.state.stopTime)
-	}
-
-	checkTimeUp(){
-		if(this.state.currentTime > this.state.stopTime){
-			this.pushNewStartTime()
-			console.log("Time's up!")
-		}
-	}
 	componentDidMount(){
-		this.fetchCurrentTime()
-		this.fetchGroupData()
-		this.fetchGroupName()
-		setInterval(() => {
-			this.setState({
-				currentTime : this.state.offset + Date.now()
-			})
-			this.checkTimeUp()
-		}, 100)
-		//Boss part
-		setTimeout(function(){
-			this.collectPeople()
-			}.bind(this),1000);
-		setTimeout(function(){
-			this.countPeople()
-			}.bind(this),2000);
+		this.collectPeople()
 	}
 
 	render(){
